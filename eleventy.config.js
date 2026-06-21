898011e4bc4cb64f71140646c295e17ef8aba269
Nguyenduc-Aduca <aduca.daotao@gmail.com>
2026-06-22 01:28:48 +0700

    Add files via upload

---
+++ eleventy.config.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/styles.css");
+  eleventyConfig.addPassthroughCopy("src/script.js");
+  eleventyConfig.addPassthroughCopy("src/admin");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
